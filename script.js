af7b4c44c6b50f238cd8875e0c27a8d88fd8b17d
rockey <rockeysen000@gmail.com>
2024-03-06 14:11:36 +0530

    added script and page #2

--- script.js
+++ script.js
@@ -1,22 +1,60 @@
-var videocon = document.querySelector("#video-container")
-var playbtn = document.querySelector("#play")
-videocon.addEventListener("mouseenter", function () {
-  gsap.to(playbtn, {
-    scale: 1,
-    opacity: 1
-  })
-})
 
-videocon.addEventListener("mouseleave", function () {
-  gsap.to(playbtn, {
-    scale: 0,
-    opacity: 0
-  })
-})
+const scroll = new LocomotiveScroll({
+  el: document.querySelector('#main'),
+  smooth: true
+});
 
-videocon.addEventListener("mousemove", function (dets) {
-  gsap.to(playbtn, {
-    left: dets.x - 50,
-    top: dets.y - 80
+function videoConAnimation() {
+  var videocon = document.querySelector("#video-container")
+  var playbtn = document.querySelector("#play")
+  videocon.addEventListener("mouseenter", function () {
+    gsap.to(playbtn, {
+      scale: 1,
+      opacity: 1
+    })
+  })
+
+  videocon.addEventListener("mouseleave", function () {
+    gsap.to(playbtn, {
+      scale: 0,
+      opacity: 0
+    })
+  })
+
+  videocon.addEventListener("mousemove", function (dets) {
+    gsap.to(playbtn, {
+      left: dets.x - 50,
+      top: dets.y - 80
+    })
+  })
+}
+videoConAnimation()
+
+function loadingAnimation() {
+  gsap.from("#page1 h1", {
+    y: 100,
+    opacity: 0,
+    delay: 0.5,
+    duration: 0.5,
+    stagger: 0.3
+
+  })
+
+  gsap.from("#page1 #video-container", {
+    scale: 0.9,
+    opacity: 0,
+    delay: 1.2,
+    duration: 0.3,
+
+  })
+}
+
+loadingAnimation()
+
+document.addEventListener("mousemove", function
+  (dets) {
+  gsap.to("#cursor", {
+    left: dets.x,
+    top: dets.y
   })
 })
